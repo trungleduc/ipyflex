b7f5935c7ab25cea45126c785405bea2a6bc8c83
Duc Trung LE <leductrungxf@gmail.com>
2021-12-28 00:58:09 +0100

    Add `galata` UI tests (#27)
    * Add ui tests
    
    * Reactivate all tests
    
    * Add reference snapshots

---
+++ ui-tests/tests/ipyflex.test.ts
@@ -0,0 +1,37 @@
+import { galata, IJupyterLabPageFixture, test } from "@jupyterlab/galata";
+import { expect } from "@playwright/test";
+
+async function renderNotebook(fileName: string, page: IJupyterLabPageFixture) {
+  const fullName = `./${fileName}.ipynb`;
+  await page.notebook.openByPath(fullName);
+  await page.notebook.activate(fullName);
+  await page.notebook.run();
+  await page.notebook.waitForRun();
+  const dashboard = await page.$("div.lm-Widget.p-Widget.custom-widget");
+  await new Promise((_) => setTimeout(_, 1000));
+  expect(await dashboard.screenshot()).toMatchSnapshot({
+    name: `${fileName}.png`,
+  });
+}
+
+const notebookList = [
+  "CustomHeader",
+  "CustomStyle",
+  "DashboardWithHeader",
+  "EmptyDashboard",
+  "MultipleSections",
+  "PlaceholderWidgets",
+  "WithWidgets",
+  "WithWidgetsFactory"
+];
+
+test.describe("ipyflex Visual Regression", () => {
+  test.beforeEach(async ({ page }) => {
+    page.setViewportSize({ width: 1280, height: 720 });
+  });
+  for (const name of notebookList) {
+    test(`Render ${name}`, async ({ page }) => {
+      await renderNotebook(name, page);
+    });
+  }
+});
