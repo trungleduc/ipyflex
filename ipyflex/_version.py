1b11ddf80c9dc950cd0dc4eae6ce0dbe1e167322
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2021-12-27 18:14:07 +0000

    Publish 0.2.0.dev2
    SHA256 hashes:
    
    index.js: 3be7592a8bbdd1d093a78ee1b365a7305fccfb8b2c1f27d4dd183113bc516634
    
    index.js.LICENSE.txt: 92c5dd3a93d232008627a7989dd980511ed2361e630aedba8ce0ca25547227f2
    
    index.js.map: 7d2b9e27e1c8690511f89634818231d98257696345be2b9dab0ee4be223ebb10
    
    ipyflex-0.2.0-dev.1.tgz: 8763ab7b808728a99999dd205df4dbe76a699bf714ab9889ff551ce72c9ad2cc
    
    ipyflex-0.2.0.dev2-py2.py3-none-any.whl: f4abf5480773a2ac64513f7893dfbe3c0d067d537c8e329674bfeb1c9c627ae3
    
    ipyflex-0.2.0.dev2.tar.gz: 2144fe1fc383c3349f18eb30da508f105bff7f0c994c8207ec9fdeb537149fbd

--- ipyflex/_version.py
+++ ipyflex/_version.py
@@ -4,7 +4,7 @@
 # Copyright (c) Trung Le.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 2, 0, ".dev", "1")
+version_info = (0, 2, 0, ".dev", "2")
 post = ''
 if len(version_info) > 3:
     post = ''.join(version_info[3:])
