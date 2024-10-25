989d2b42a3922ff2e0d707542889b9d59b239f90
Duc Trung Le <leductrungxf@gmail.com>
2024-10-25 17:16:06 +0200

    Lint

--- ipyflex/nbextension/extension.js
+++ ipyflex/nbextension/extension.js
@@ -1,17 +1,17 @@
 // Entry point for the notebook bundle containing custom model definitions.
 //
-define(function() {
-    "use strict";
+define(function () {
+  'use strict';
 
-    window['requirejs'].config({
-        map: {
-            '*': {
-                'ipyflex': 'nbextensions/ipyflex/index',
-            },
-        }
-    });
-    // Export the required load_ipython_extension function
-    return {
-        load_ipython_extension : function() {}
-    };
+  window['requirejs'].config({
+    map: {
+      '*': {
+        ipyflex: 'nbextensions/ipyflex/index',
+      },
+    },
+  });
+  // Export the required load_ipython_extension function
+  return {
+    load_ipython_extension: function () {},
+  };
 });
