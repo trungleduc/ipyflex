c42c5c11d1771f21946e25855f0d3abdd2d1e59f
Duc Trung Le <leductrungxf@gmail.com>
2024-10-25 16:53:03 +0200

    Build with lab 4

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -3,7 +3,7 @@ var baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 module.exports = {
   ...baseConfig,
   expect: {
-    toMatchSnapshot: { threshold: 0.33 },
+    toMatchSnapshot: { threshold: 0.03 },
   },
   preserveOutput: 'failures-only', 
   retries: 0
